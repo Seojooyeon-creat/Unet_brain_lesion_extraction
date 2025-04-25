79db06b603ec0a4658092cb4c245d7d785901120
Seojooyeon-creat <79130628+Seojooyeon-creat@users.noreply.github.com>
2025-04-25 21:33:03 +0900

    Create dataset.py
    add code

---
+++ model/dataset.py
@@ -0,0 +1,25 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+import torchvision.transforms as T
+
+class BrainTumorDataset(Dataset):
+    def __init__(self, img_dir, mask_dir, transform=None):
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = sorted(os.listdir(img_dir))
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
+        image = Image.open(img_path).convert("L")
+        mask = Image.open(mask_path).convert("L")
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+        return image, mask
